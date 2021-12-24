6382ae38592f7b7193716f6924feff0728130122
Song San <songsan2012@gmail.com>
2021-12-24 06:49:39 -0800

    Added Who Founded buttons

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel1: UILabel!
+    @IBOutlet weak var messageLabel2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // -- Clear the labels
+        messageLabel1.text = ""
+        messageLabel2.text = ""
+        
     }
 
-
+    // -- Pressed the Google Button
+    @IBAction func FoundedGoogleButtonPressed(_ sender: Any) {
+        
+        print("😀 Google Button Pressed.")
+        messageLabel1.text = "Larry Page"
+        messageLabel2.text = "Sergey Brin"
+        
+    }
+    
+    // -- Pressed the Rent Button
+    @IBAction func FoundedRentButtonPressed(_ sender: Any) {
+        
+        print("😁 Rent Button Pressed.")
+        
+        messageLabel1.text = "Jennifer Hyman"
+        messageLabel2.text = "Jenny Fleiss"
+    }
+    
+    
+    // -- Pressed the Clear Button
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        
+        // -- Clear the labels
+        messageLabel1.text = ""
+        messageLabel2.text = ""
+    }
+    
+    
 }
 
